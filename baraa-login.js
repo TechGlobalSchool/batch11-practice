f4f7d4ec8987507518a92af23613bf11f508bd54
beldeirawi01 <beldeirawi@gmail.com>
2025-02-18 21:11:06 -0600

    conflict

--- baraa-login.js
+++ baraa-login.js
@@ -5,4 +5,4 @@ console.log('This is a newly added line of code');
 console.log('Trying to get a conflict')
 
 
-crossOriginIsolated.log('Will I get a conflict?')
+console.log('Will I get a conflict?');
