575c432635c45543e325bb17316f56541d5f47ae
mhishmeh <hishmoe@gmail.com>
2025-02-18 20:58:34 -0600

    moe and moe edits

--- moehishmeh-login.js
+++ moehishmeh-login.js
@@ -1,2 +1,4 @@
 console.log('this is mo(hamad)e hishmayyyyyyyyy')
 console.log('hello')
+
+//hello hello hello
--- mohammadhajeer-login.js
+++ mohammadhajeer-login.js
@@ -1,3 +1,4 @@
 console.log(`This is Mohammad Hajeer!`)
 console.log('Hello, I am trying to make trouble!');
 
+//heloo lhello hello
