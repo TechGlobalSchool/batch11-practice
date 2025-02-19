1a761ae0e8512f5027cc00298aa01771f77bff70
volkan.tutunculer <volkan.tutunculer@gmail.com>
2025-02-18 22:09:48 -0500

    volkan Update

--- volkan_login.js
+++ volkan_login.js
@@ -3,3 +3,5 @@ console.log('It is Volkan Login page');
 console.log('Hello, I am trying to make trouble!');
 console.log('i got your trouble!');
 
+console.log('It is Volkan Login page Test');
+console.log('It is Volkan Login page Test 2');
