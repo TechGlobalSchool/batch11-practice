c6907d9fab7f9a733d5a23c283edc597ecfe7534
alina-sadykova <asadykovaa@gmail.com>
2025-02-18 20:42:27 -0600

    updated the tests

--- alina-payment-login-test.js
+++ alina-payment-login-test.js
@@ -3,3 +3,9 @@ console.log("Test payment login page");
 console.log("Hello, I am trying to make trouble!");
 // Add a new test
 console.log("New tests");
+// Add a new test
+console.log("New tests");
+// Add a new test
+console.log("New tests");
+// Add a new test
+console.log("New tests");
