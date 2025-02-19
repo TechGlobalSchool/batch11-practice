20bc1fa05d69dae0b41952dbac785ce9d63c96aa
Omar-Yehya <143041754+Omar-Yehya@users.noreply.github.com>
2025-02-18 21:09:05 -0600

    newfunc

--- omary.js
+++ omary.js
@@ -3,7 +3,11 @@ console.log("hi")
 
 console.log('no')
 
-console.log("hi, im a goofball, call me goofy")
-console.log('Hello, I am trying to make trouble!');
+function saysHi(){
+    console.log('hi')
+}
 
+function saysHello(){
+    console.log('Hello')
+}
 
