0595a5cebeaea0443fa3e32cd7cad6d5e9122789
HAJEEMOH000 <mohammadnhajeer@gmail.com>
2025-02-27 13:59:34 -0600

    Added new smoke tests

--- mohammadhajeer-login.js
+++ mohammadhajeer-login.js
@@ -7,3 +7,12 @@ console.log('ALL FIXED!')
 
 //heloo lhello hello
 // JOHN
+
+for (let i = 0; i < 10; i++)
+    console.log(`I am the best at coding and ping pong.`)
+
+
+let num = false
+if (num || false || num || true || false || false) {
+    console.log(`Not a trick question! It's true!`)
+}
